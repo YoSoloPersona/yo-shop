33beee84b5b57b2c83bd9b07776577a82f097c6a
YoSoloPersona <79009569+YoSoloPersona@users.noreply.github.com>
2022-07-13 20:27:39 +0300

    Реализован базовый запуск express сервера

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,11 +2,12 @@ import express from 'express';
 import debug from 'debug';
 
 const app = express();
-const port = 3000;
+
+const PORT = process.env.PORT || 3000;
 
 // Протоколы
 const log = debug('app:log');
 
-app.listen(port, () => {
-    log(`Сервер запущен на порту: ${port}`);
+app.listen(PORT, () => {
+    log(`Сервер запущен на порту: ${PORT}`);
 });
