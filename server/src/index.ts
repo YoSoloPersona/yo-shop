beec71a61ed7e3e7a87167a8c32b63c275ee015e
YoSoloPersona <79009569+YoSoloPersona@users.noreply.github.com>
2022-07-14 21:16:18 +0300

    Добавил конетейнер БД и админки БД

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 3000;
 // Протоколы
 const log = debug('app:log');
 
+// Запускаем сервер
 app.listen(PORT, () => {
     log(`Сервер запущен на порту: ${PORT}`);
 });
