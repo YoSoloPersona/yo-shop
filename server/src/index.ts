2f0181d5f8cf5ae1a742379e72707a7aa14d9322
YoSoloPersona <79009569+YoSoloPersona@users.noreply.github.com>
2022-10-28 14:53:18 +0300

    Добавил протоколирование morgan

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import debug from 'debug';
 import cors from 'cors';
+import morgan from 'morgan';
 
 // local
 import { router as routerApi } from './routers/routerApi';
@@ -16,6 +17,7 @@ const logError = debug('app:error');
 const app = express();
 
 // Middleware
+app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
 app.use(domain.api.url, routerApi);
