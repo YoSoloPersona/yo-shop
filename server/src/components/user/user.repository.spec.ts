15227ac79f3ee05c1536044a9b88ed5fc6c16a5c
YoSoloPersona <79009569+YoSoloPersona@users.noreply.github.com>
2023-10-31 23:04:27 +0300

    test(server) add test for user repository

---
+++ server/src/components/user/user.repository.spec.ts
@@ -0,0 +1,68 @@
+import jasmine from 'jasmine';
+import { User } from 'yo-shop-model';
+
+// locals
+import { UserRepository } from './user.repository';
+import { db } from '../../db';
+
+describe('#userRepository', () => {
+    // root
+    const root: User = {
+        email: 'yo.solo.persona@gmail.com',
+        password: '1234',
+        role: 'root'
+    };
+
+    // admin
+    const admin: User = {
+        email: 'admin@mail.ru',
+        password: '1234',
+        role: 'admin'
+    };
+
+    // user
+    const user1: User = {
+        email: 'user1@mail.ru',
+        password: '1234',
+        role: 'user'
+    };
+
+    let userRepository: UserRepository;
+
+    beforeAll(async () => {
+        await db();
+        userRepository = new UserRepository();
+    });
+
+    it('create read update delete', async () => {
+        // Arrange
+
+        // Act
+        const createdUser = await userRepository.create(user1); // create
+        const findedUser = await userRepository.readById(createdUser.id ?? -1); // read
+        const affectedCount = await userRepository.update(
+            createdUser.id ?? -1,
+            { email: 'user1New@mail.ru' }
+        ); // update
+        const deletedCount = await userRepository.deleteById(
+            createdUser.id ?? -1
+        ); // delete
+
+        // Assert
+        expect(createdUser).toBeDefined();
+        expect(createdUser.id).toBeGreaterThan(0);
+        expect(findedUser).toEqual(createdUser);
+        expect(affectedCount).toEqual(1);
+        expect(deletedCount).toEqual(1);
+    });
+
+    it('create empty user', async () => {
+        await expectAsync(userRepository.create({})).toBeRejected();
+    });
+
+    it('read non-existent user', async () => {
+        const findedUser = await userRepository.readByEmail('non.existent.email@mail.ru');
+
+        expect(findedUser).toBeUndefined();
+    })
+});
