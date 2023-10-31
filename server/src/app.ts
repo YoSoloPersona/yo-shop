fe5ec4e61f9e077cdbc20b71227f5c8dd974640e
YoSoloPersona <79009569+YoSoloPersona@users.noreply.github.com>
2023-10-31 23:05:57 +0300

    feature(server) add method addController

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,32 +1,132 @@
-import express from 'express';
+import express, { Router } from 'express';
 import { Server } from 'node:http';
 import debug from 'debug';
 import cors from 'cors';
 
 // locals
 import config from './config';
-import { router as routerApi } from './routers/routerApi';
 import { domain } from './helpers/domain';
 import { errorHandler } from './middleware/errorHandler';
+import { MetaController, MetaGet, symbols } from './helpers/decorators';
+import { getMeta } from './helpers/decorators/meta';
 
-// Protocols
+// protocols
 const log = debug('app:log');
 const error = debug('app:error');
 
-// Server
+// server
 const app = express();
 
-// Middleware
+// middleware
 app.use(cors());
 app.use(express.json());
-app.use(domain.api.url, routerApi);
 app.use(errorHandler);
 
-// start server
+/**
+ * Start server.
+ * @returns Promise<Server>
+ */
 export function start(): Promise<Server> {
     return Promise.resolve(
         app.listen(config.server.port, () => {
             log(`server starts on port: ${config.server.port}`);
         })
     );
+}
+
+/**
+ *
+ * @param classConstructor
+ * @returns
+ */
+export function addController(
+    classConstructor: new (...args: any) => any
+): Promise<void> {
+    // if no constructor is passed
+    if (!classConstructor) {
+        return Promise.resolve();
+    }
+
+    const { path: pathController } = getMeta<MetaController>(
+        classConstructor,
+        symbols.Controller
+    );
+
+    // if there is no metadata
+    if (!pathController || typeof pathController !== 'string') {
+        return Promise.resolve();
+    }
+
+    const router = Router();
+    const controller = new classConstructor();
+
+    log(`added controller ${classConstructor.name}`);
+
+    Reflect.ownKeys(classConstructor.prototype)
+        .map(prop => classConstructor.prototype[prop])
+        .filter(
+            prop => prop instanceof Function && getMeta(prop, symbols.Method)
+        )
+        .map<[Function, MetaGet]>(method => [
+            method,
+            getMeta(method, symbols.Method)
+        ])
+        .forEach(([classMethod, { httpMethod, path: pathMethod, params }]) => {
+            // регистрируем обработчик
+            switch (httpMethod) {
+                case 'get':
+                    router[httpMethod](
+                        `${pathController}${pathMethod ?? ''}${params}`,
+                        (req, res, next) => {
+                            const result = classMethod.call(
+                                controller,
+                                params ? req.params : undefined
+                            );
+
+                            if (result instanceof Promise) {
+                                result
+                                    .then(promiseResult => {
+                                        res.send(promiseResult);
+                                    })
+                                    .catch(err => next(err));
+                            } else {
+                                res.send(result.toString());
+                            }
+                        }
+                    );
+                    break;
+
+                case 'post':
+                    router[httpMethod](
+                        // address
+                        `${pathController}${pathMethod ?? ''}${params ?? ''}`,
+                        // handler
+                        (req, res, next) => {
+                            const result = classMethod.call(
+                                controller,
+                                req.body
+                            );
+
+                            if (result instanceof Promise) {
+                                result
+                                    .then(promiseResult => {
+                                        res.send(promiseResult);
+                                    })
+                                    .catch(err => next(err));
+                            } else {
+                                res.send(result.toString());
+                            }
+                        }
+                    );
+                    break;
+            }
+
+            log(
+                `Добавлен обработчик ${classConstructor.name}.${classMethod.name}(${httpMethod})`
+            );
+        });
+
+    app.use(router);
+
+    return Promise.resolve();
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,8 +2,9 @@
 import debug from 'debug';
 
 // locals
-import { db } from './db/db';
-import { start } from './app';
+import { db } from './db';
+import { addController, start } from './app';
+import { ControllerUser } from './components/user';
 
 // protocols
 const log = debug('app:log');
@@ -11,6 +12,8 @@ const error = debug('app:error');0
  
 // init db
 db()
+// add controllers
+.then(() => addController(ControllerUser))
 // start server
 .then(() => start())
 // catch errors
