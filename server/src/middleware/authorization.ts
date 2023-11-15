83992ed7b3f918eeb12e36024587b85635613435
YoSoloPersona <79009569+YoSoloPersona@users.noreply.github.com>
2023-11-15 22:48:50 +0300

    refractor(server) add error logging

--- server/src/middleware/authorization.ts
+++ server/src/middleware/authorization.ts
@@ -1,4 +1,4 @@
-import { Response, NextFunction } from 'express';
+import { Response, NextFunction, RequestHandler } from 'express';
 import { Role } from 'yo-shop-model';
 import debug from 'debug';
 
@@ -12,10 +12,10 @@ const error = debug('middleware:authorization:error');
 
 /**
  * Creates a function to check if you have permissions to perform an operation
- * @param role List of user roles for which access to query execution is allowed
+ * @param roles List of user roles for which access to query execution is allowed
  * @returns (req: Request, res: Response, next: NextFunction) => void.
  */
-export function role(...role: Role[]) {
+export function authorization(...roles: Role[]): RequestHandler {
     /**
      * Checks if you have permissions to perform the operation.
      * @param req request
@@ -23,17 +23,18 @@ export function role(...role: Role[]) {
      * @param next next handler
      * @returns void.
      */
-    return function authorization(
+    return function (
         req: RequestAuthentication,
         res: Response,
         next: NextFunction
     ) {
         if (!req.user) {
-            
+            error('User is not authenticated');
             return next(ErrorApi.unauthorized('User is not authenticated'));
         }
 
-        if (!role.includes(req.user.role)) {
+        if (!roles.includes(req.user.role)) {
+            error('Not enough rights to execute the request');
             return next(
                 ErrorApi.forbidden('Not enough rights to execute the request')
             );
