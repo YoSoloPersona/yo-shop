bb58a8a6f83484d6e6a70c5bcb49e435fceafe86
YoSoloPersona <79009569+YoSoloPersona@users.noreply.github.com>
2024-03-06 01:46:15 +0300

    test(rest-api): create category with the user role

--- rest-api/src/category/repository.category.spec.ts
+++ rest-api/src/category/repository.category.spec.ts
@@ -1,37 +1,79 @@
 import 'jasmine';
 
 // locals
-import { RepositoryCategory } from '..';
+import { RepositoryCategory, RepositoryUser } from '..';
 import { admin, root, user1 } from '../test/data/users';
+import { User } from '@YoSoloPersona/yo-shop-model';
 
 // timeout
 const timeoutIt = 5000;
 
-xdescribe('#RepositoryCategory', () => {
+describe('#RepositoryCategory', () => {
     let repositoryCategory: RepositoryCategory;
 
     beforeAll(() => {
-        repositoryCategory = new RepositoryCategory({});
+        repositoryCategory = new RepositoryCategory({
+            host: process.env.SHOP_HOST || 'localhost',
+            port: Number(process.env.SHOP_PORT) || 3000
+        });
     });
 
-    xit(
+    it(
         'crud',
         async () => {
             // arrange
-            const categoruName = 'Category 1';
             await repositoryCategory.authorization({
-                email: root.email,
-                password: root.password
+                email: process.env.SHOP_USER || '',
+                password: process.env.SHOP_PASSWORD || ''
             }); // login as root
 
             // act
             const category = await repositoryCategory.create({
-                name: categoruName
+                name: 'Category 1'
             });
 
             // arrange
             expect(category).toBeDefined();
             expect(category.name).toEqual('Категория1');
+        },
+        timeoutIt
+    );
+
+    it(
+        'create with the user role',
+        async () => {
+            // arrange
+            const repositoryUser = new RepositoryUser({
+                host: process.env.SHOP_HOST || 'localhost',
+                port: Number(process.env.SHOP_PORT) || 3000
+            });
+            await repositoryUser.authorization({
+                email: process.env.SHOP_USER || '',
+                password: process.env.SHOP_PASSWORD || ''
+            });
+
+            // prepare the user for authorization
+            const man: Omit<User, 'role'> = {
+                email: 'user@gmail.com',
+                password: '1234'
+            };
+            const { token } = await repositoryUser.registration(man);
+
+            const repositoryUserMan = new RepositoryUser({
+                host: process.env.SHOP_HOST || 'localhost',
+                port: Number(process.env.SHOP_PORT) || 3000
+            });
+            
+
+            // Act
+            await repositoryUserMan.authorization(man);
+
+            // try adding a category with insufficient permissions
+            expectAsync(await repositoryCategory.create({
+                name: ''
+            })).toBeRejected()
+
+
         },
         timeoutIt
     );
